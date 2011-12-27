80f57316a23f84b74c370a6967dae3c19a5448cb
unknown <anlagmat@IOSC004.iosevilladc>
2011-12-27 10:27:11 +0100

    Initial commit with the current state of the project.
    We will start from here.

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+app.listen(8080);
+var llamadas = [];
+
+/*Rutas*/
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+app.get('/encola/:clid/:did/:cola/:uniqueid', function(req, res){
+    console.log('Ha entrado una en cola');
+    llamada = {
+        id: req.params.uniqueid,
+        origen: req.params.clid,
+        destino: req.params.did,
+        cola: req.params.cola,
+        inicio: new Date
+    };
+    io.sockets.emit('encola', llamada);
+    
+    llamadas.push(llamada);
+    res.send('ok');
+});
+app.get('/respondida/:uniqueid/:extension', function(req, res){
+    console.log('Sale una de la cola');
+    io.sockets.emit('respondida', {
+        id: req.params.uniqueid,
+        extension: req.params.extension
+    });
+    deleteFromArray(llamadas,req.params.uniqueid);
+    res.send('ok');
+});
+function deleteFromArray(my_array, element) {
+    position = my_array.indexOf(element);
+    my_array.splice(position, 1);
+}
+io.disable('heartbeats');
+io.sockets.on('connection', function (socket) {
+    /* Si llega alguien nuevo, le mandamos el estado actual de llamadas y agentes */
+    if (llamadas.length > 0){
+        io.sockets.socket(socket.id).emit('llamadas', llamadas);
+    }
+});
---
+++ jquery.stopwatch.js
@@ -0,0 +1,129 @@
+(function( $ ){
+
+    function incrementer(ct, increment) {
+        return function() { ct+=increment; return ct; };
+    }
+    
+    function pad2(number) {
+         return (number < 10 ? '0' : '') + number;
+    }
+    
+    function formatMilliseconds(millis) {
+        var x, seconds, minutes, hours;
+        x = millis / 1000;
+        seconds = Math.floor(x % 60);
+        x /= 60;
+        minutes = Math.floor(x % 60);
+        x /= 60;
+        hours = Math.floor(x % 24);
+        // x /= 24;
+        // days = Math.floor(x);
+        return [pad2(hours), pad2(minutes), pad2(seconds)].join(':');
+    }
+    
+    var methods = {
+        
+        init: function(options) {
+            var settings = {
+                updateInterval: 1000, 
+                startTime: 0, 
+                formatter: formatMilliseconds
+            };
+            
+            if (options) { $.extend(settings, options); }
+            
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                
+                // If the plugin hasn't been initialized yet
+                if (!data) {
+                    // Setup the stopwatch data
+                    data = settings;
+                    data.target = $this;
+                    data.elapsed = settings.startTime;
+                    // create counter
+                    data.incrementer = incrementer(data.startTime, data.updateInterval);
+                    data.tick_function = function() {
+                        var millis = data.incrementer();
+                        data.elapsed = millis;
+                        data.target.trigger('tick.stopwatch', [millis]);
+                        data.target.stopwatch('render');
+                    };
+                    $this.data('stopwatch', data);
+                }
+                
+            });
+        },
+        
+        start: function() {
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                // Mark as active
+                data.active = true;
+                data.timerID = setInterval(data.tick_function, data.updateInterval)
+                $this.data('stopwatch', data);
+            });
+        },
+        
+        stop: function() {
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                clearInterval(data.timerID);
+                data.active = false;
+                $this.data('stopwatch', data);
+            });
+        },
+        
+        destroy: function() {
+            return this.each(function(){
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                $this.stopwatch('stop').unbind('.stopwatch').removeData('stopwatch');                
+            })
+        },
+        
+        render: function() {
+            var $this = $(this),
+                data = $this.data('stopwatch');
+            $this.html(data.formatter(data.elapsed));
+        },
+        
+        toggle: function() {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data('stopwatch');
+                if (data.active) {
+                    $this.stopwatch('stop');
+                } else {
+                    $this.stopwatch('start');
+                }
+            });
+        }, 
+        
+        reset: function() {
+            return this.each(function() {
+                var $this = $(this);
+                    data = $this.data('stopwatch');
+                data.incrementer = incrementer(data.startTime, data.updateInterval);
+                data.elapsed = data.startTime;
+                $this.data('stopwatch', data);
+            });
+        }
+    };
+    
+    
+    // Define the function
+    $.fn.stopwatch = function( method ) {
+        if (methods[method]) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.stopwatch' );
+        } 
+    };
+
+})( jQuery );
